6b98da218d963c142753100e2f6a6997546fb018
hadi0005 <hocine.hadid@etudiant.univ-reims.fr>
2021-12-16 21:27:39 +0100

    internationalisation fini

--- app/src/main/java/com/hocine/fotoshare/OptionsActivity.java
+++ app/src/main/java/com/hocine/fotoshare/OptionsActivity.java
@@ -12,14 +12,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class OptionsActivity extends AppCompatActivity {
 
-    TextView logout, settings;
+    TextView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
         logout = findViewById(R.id.logout);
-        settings = findViewById(R.id.settings);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
